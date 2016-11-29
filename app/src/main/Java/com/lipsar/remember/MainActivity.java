947579001721aea23439f3e61081ddc8f934dfa0
Lipsar <varlapovs@bk.ru>
2016-11-29 19:07:29 +0300

    Create MainActivity class and layout

---
+++ app/src/main/Java/com/lipsar/remember/MainActivity.java
@@ -0,0 +1,20 @@
+package com.lipsar.remember;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import lipsar.remember.R;
+
+/**
+ * Created by Lipsar on 29.11.2016.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
