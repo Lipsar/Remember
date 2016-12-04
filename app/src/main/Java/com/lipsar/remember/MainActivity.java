52b9ff83b1a7bfcfe15a1a4191a43fc77d4432a0
Lipsar <varlapovs@bk.ru>
2016-12-04 18:40:31 +0300

    Start to implementing NavigationDrawer

--- app/src/main/Java/com/lipsar/remember/MainActivity.java
+++ app/src/main/Java/com/lipsar/remember/MainActivity.java
@@ -2,17 +2,19 @@ package com.lipsar.remember;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import lipsar.remember.R;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
